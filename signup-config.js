904cdc645079988ca41c9fe16495ec54bdfd356e
justus <justusidodo@gmail.com>
2026-04-02 20:37:36 +0100

    third

--- signup-config.js
+++ signup-config.js
@@ -1,3 +1,3 @@
 // Browser-only signup code for static hosting.
 // Warning: this value is public in a static site, so do not treat it as a secure secret.
-window.ADMIN_SIGNUP_CODE = window.ADMIN_SIGNUP_CODE || '';
+window.ADMIN_SIGNUP_CODE = 'idojuan-301-203';
